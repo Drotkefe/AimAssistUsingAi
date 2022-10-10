920c2955ec11ce143f6836785b3eb5becc308362
™kukec™ ™kukec™ <kukimsd@gmail.com>
2022-10-10 21:28:37 +0200

    added circle to calculate

--- Project work.py
+++ Project work.py
@@ -1,4 +1,3 @@
-from multiprocessing.connection import wait
 import time
 import math
 import win32api
@@ -8,10 +7,6 @@ import mss
 import numpy as np
 import torch
 import random
-import scipy
-from scipy import stats,interpolate
-import pyautogui
-import pydirectinput
 
 def Closest_enemy_head(list):
     pos=win32api.GetCursorPos()
@@ -59,10 +54,12 @@ def cursor_move(x,y):
 model=torch.hub.load('ultralytics/yolov5','custom',path='C:/Users/Patrik/Desktop/best.pt')
 
 with mss.mss() as sct:
-    monitor = {"top": 225, "left": 450, "width": 1020, "height": 630}
-    #monitor = {"top": 0, "left": 0, "width": 900, "height": 630}
+    #monitor = {"top": 225, "left": 450, "width": 1020, "height": 630}
+    monitor = {"top": 0, "left": 0, "width": 1920, "height": 1080}
 while True:
+    t1 = time.time()
     img=np.array(sct.grab(monitor))
+    cv2.circle(img,(960,540),5,(255,0,0),-1)
     result=model(img)
     rl=result.xyxy[0].tolist()
     if len(rl)>0:
@@ -72,10 +69,18 @@ while True:
         if dest==None:
             dest=Closest_enemy(rl)
         else:
-            cursor_move(int(dest[0]),int(dest[1]))
-            
-    cv2.imshow('debug',np.squeeze(result.render()))
+            cursor_move(int(dest[0]),int(dest[1]))  
+    t2 = time.time()
+    times=[]
+    times.append(t2-t1)
+    times = times[-50:]
+    ms = sum(times)/len(times)*1000
+    fps = 1000 / ms
+    print("FPS", fps)
+    cv2.circle(img,(500,600),50,(255,0,0),-1)
+    cv2.imshow('debug',np.squeeze(result.render()),)
     cv2.waitKey(1)
+    
     if keyboard.is_pressed('q'):
         break
 cv2.destroyAllWindows()
